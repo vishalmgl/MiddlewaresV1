63319c4b95ba844733af1f74ff324bf343e4a299
vishalmgl <117003133+vishalmgl@users.noreply.github.com>
2025-04-20 17:04:33 +0530

    Add project files.

---
+++ Middlewares/MyCustomMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace Middlewares
+{
+    public class MyCustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MyCustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("Before request");
+            await _next(context); // Call the next middleware or controller
+            Console.WriteLine("After request");
+        }
+    }
+
+}
